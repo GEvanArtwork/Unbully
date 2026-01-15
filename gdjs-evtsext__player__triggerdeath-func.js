74cd96714f27e3f537febf8811b87005f59f9f2f
GEvanArtwork <gevan.artwork@gmail.com>
2026-01-15 12:02:13 +0000

    Add files via upload

---
+++ gdjs-evtsext__player__triggerdeath-func.js
@@ -0,0 +1,104 @@
+
+if (typeof gdjs.evtsExt__Player__TriggerDeath !== "undefined") {
+  gdjs.evtsExt__Player__TriggerDeath.registeredGdjsCallbacks.forEach(callback =>
+    gdjs._unregisterCallback(callback)
+  );
+}
+
+gdjs.evtsExt__Player__TriggerDeath = {};
+gdjs.evtsExt__Player__TriggerDeath.idToCallbackMap = new Map();
+gdjs.evtsExt__Player__TriggerDeath.GDPlayerObjects1= [];
+gdjs.evtsExt__Player__TriggerDeath.GDPlayerObjects2= [];
+
+
+gdjs.evtsExt__Player__TriggerDeath.eventsList0 = function(runtimeScene, eventsFunctionContext) {
+
+{
+
+
+
+}
+
+
+{
+
+
+let isConditionTrue_0 = false;
+{
+{/* Unknown instruction - skipped. */}
+}
+
+}
+
+
+};
+
+gdjs.evtsExt__Player__TriggerDeath.func = function(runtimeScene, Player, parentEventsFunctionContext) {
+let scopeInstanceContainer = null;
+var eventsFunctionContext = {
+  _objectsMap: {
+"Player": Player
+},
+  _objectArraysMap: {
+"Player": gdjs.objectsListsToArray(Player)
+},
+  _behaviorNamesMap: {
+},
+  globalVariablesForExtension: runtimeScene.getGame().getVariablesForExtension("Player"),
+  sceneVariablesForExtension: runtimeScene.getScene().getVariablesForExtension("Player"),
+  localVariables: [],
+  getObjects: function(objectName) {
+    return eventsFunctionContext._objectArraysMap[objectName] || [];
+  },
+  getObjectsLists: function(objectName) {
+    return eventsFunctionContext._objectsMap[objectName] || null;
+  },
+  getBehaviorName: function(behaviorName) {
+    return eventsFunctionContext._behaviorNamesMap[behaviorName] || behaviorName;
+  },
+  createObject: function(objectName) {
+    const objectsList = eventsFunctionContext._objectsMap[objectName];
+    if (objectsList) {
+      const object = parentEventsFunctionContext && !(scopeInstanceContainer && scopeInstanceContainer.isObjectRegistered(objectName)) ?
+        parentEventsFunctionContext.createObject(objectsList.firstKey()) :
+        runtimeScene.createObject(objectsList.firstKey());
+      if (object) {
+        objectsList.get(objectsList.firstKey()).push(object);
+        eventsFunctionContext._objectArraysMap[objectName].push(object);
+      }
+      return object;
+    }
+    return null;
+  },
+  getInstancesCountOnScene: function(objectName) {
+    const objectsList = eventsFunctionContext._objectsMap[objectName];
+    let count = 0;
+    if (objectsList) {
+      for(const objectName in objectsList.items)
+        count += parentEventsFunctionContext && !(scopeInstanceContainer && scopeInstanceContainer.isObjectRegistered(objectName)) ?
+parentEventsFunctionContext.getInstancesCountOnScene(objectName) :
+        runtimeScene.getInstancesCountOnScene(objectName);
+    }
+    return count;
+  },
+  getLayer: function(layerName) {
+    return runtimeScene.getLayer(layerName);
+  },
+  getArgument: function(argName) {
+    return "";
+  },
+  getOnceTriggers: function() { return runtimeScene.getOnceTriggers(); }
+};
+
+gdjs.evtsExt__Player__TriggerDeath.GDPlayerObjects1.length = 0;
+gdjs.evtsExt__Player__TriggerDeath.GDPlayerObjects2.length = 0;
+
+gdjs.evtsExt__Player__TriggerDeath.eventsList0(runtimeScene, eventsFunctionContext);
+gdjs.evtsExt__Player__TriggerDeath.GDPlayerObjects1.length = 0;
+gdjs.evtsExt__Player__TriggerDeath.GDPlayerObjects2.length = 0;
+
+
+return;
+}
+
+gdjs.evtsExt__Player__TriggerDeath.registeredGdjsCallbacks = [];
